05c9e485472689d7295ae77c62610496064a6984
BhaveshPoriya <yogita@RanjitMahto.local>
2014-10-06 16:51:21 +0530

    bhavesh

--- FigureMe/AppDelegate.m
+++ FigureMe/AppDelegate.m
@@ -13,7 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-    
+    //Bhavesh
     // Handle launching from a notification
     UILocalNotification *locationNotification = [launchOptions objectForKey:UIApplicationLaunchOptionsLocalNotificationKey];
     if (locationNotification) {
